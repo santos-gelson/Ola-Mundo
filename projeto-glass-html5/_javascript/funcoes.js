3b98d4e1eff4acc68abcb2b668caf8edd2e0bdcf
santos-gelson <63474702+santos-gelson@users.noreply.github.com>
2020-04-21 20:35:29 -0300

    Atualização do dia
    Aulas de #22 a # 29

---
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,6 @@
+/* Criar uma área de script para ativar a função que muda a foto quando o mouse passa sobre o menu.(método muda foto) */
+
+function mudaFoto (foto) {
+    document.getElementById("icone").src = foto;
+}
+
